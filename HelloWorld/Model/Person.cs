43d8aab6f4d31c341dd587a48440a5db460621de
AndreGiuliani94 <57347381+AndreGiuliani94@users.noreply.github.com>
2019-11-04 12:35:19 +0100

    variabili e oggetti

---
+++ HelloWorld/Model/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Model
+{
+    class Person                                                    //definizione classe    
+    {
+        public string nome;                                                //variabili di classe
+        public string cognome;
+        public int età;
+        public string sesso;
+        public int altezza;
+        public double pesso;
+
+        public Person(string nome, string cognome)                  //definizione metodi con relativa FIRMA
+        {
+            this.nome = nome;                                       //ambiguità nome variabile, usare this. per riferirsi alla variabile di classe
+            this.cognome = cognome;
+            this.sesso = "Sconosciuto";
+        }
+
+
+        public Person(string nome)
+        {
+        }
+
+        public Person()                                             //costruttore vuoto
+        {
+        }
+
+        public string Anagrafica()
+        {
+            string anagrafica = this.nome + " " + this.cognome;
+
+            return anagrafica;
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld.Model;
+using System;
 
 namespace HelloWorld
 {
@@ -6,7 +7,53 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // value type
+
+            int intero;
+            
+            float razionale32;
+            
+            double razionale64;
+            
+            bool valoreLogico;
+            
+            char carattere;
+            
+            decimal valoreDecimale;
+
+            // reference type
+
+            string insiemeDiCaratteri;
+
+            string stringa1 = "la mia casa è bella";
+            string stringa2 = stringa1;
+
+            Console.WriteLine(stringa1);
+            Console.WriteLine(stringa2);
+
+            stringa1 = "la mia casa è brutta";
+
+            Console.WriteLine(stringa1);
+            Console.WriteLine(stringa2);
+
+            object oggetto;
+
+            Person luca = new Person("Luca", "Lippi");                     //ho ISTANZIATO un nuovo oggetto Person
+            luca.età = 18;
+
+            Console.WriteLine(luca.Anagrafica());
+
+            Person giovanni = luca;
+
+            giovanni.nome = "Giovanni";
+
+            Console.WriteLine(luca.Anagrafica());
+            Console.WriteLine(giovanni.Anagrafica());
+
+
+
+
+
         }
     }
 }
